c43decd7f83b0f59d4dda0ecff314f7d1ff7036a
daybreak1999 <h411125@nehs.hc.edu.tw>
2021-05-16 15:20:47 -0700

    add codes

---
+++ hw3/rpc_function.py
@@ -0,0 +1,23 @@
+import serial
+import time
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev, 9600)
+
+s.write(bytes("\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+
+while 1:
+    s.write(bytes("/functionchange/run 1\r", 'UTF-8'))
+    line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+    print(line)
+    line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+    print(line)
+    time.sleep(1)
+
+
+
+s.close()
